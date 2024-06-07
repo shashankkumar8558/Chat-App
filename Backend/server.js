8cb45b01e29719058dcb822046a325e388d9800e
shashank <shashankkumar8558@gmail.com>
2024-06-08 01:34:16 +0530

    Deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import { mongooseConnection } from "./DB/MongoDB.js";
 import authRouter from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js"
@@ -6,7 +7,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
-
+const __dirname = path.resolve();
 
 app.use(express.json()); //To parse the incoming requests with JSON payloads from (req.body)
 app.use(cookieParser());
@@ -15,10 +16,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRoute);
 app.use('/api/users', userRoute);
 
-app.get('/', function (req, res, next) {
-  res.send("Hello Bhai");
-})
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 server.listen(5000, () => {
   mongooseConnection();
